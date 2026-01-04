176f092c41eafd159d900ec6dbb0f66bc121119d
Gabriel Lopes <gabriel-fernando2008@hotmail.com>
2026-01-04 14:12:45 -0300

    eletronicacenter

---
+++ eletronicacenterbackend.model/Model/barras_led.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eletronicacenterbackend.model.Model
+{
+    public class barras_led
+    {
+        public int id_barra { get; set; }
+        public string modelo { get; set; }
+    }
+}
