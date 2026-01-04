2083e2f005c02e59626412801f4830fd0351b876
Gabriel Lopes <gabriel-fernando2008@hotmail.com>
2026-01-04 16:09:43 -0300

    completando a model barras_led

--- eletronicacenterbackend.model/Model/barras_led.cs
+++ eletronicacenterbackend.model/Model/barras_led.cs
@@ -10,5 +10,20 @@ namespace eletronicacenterbackend.model.Model
     {
         public int id_barra { get; set; }
         public string modelo { get; set; }
+        public decimal comprimento_cm { get; set; }
+        public int quantidade_leds { get; set; }
+
+        public string codigo_fabricante { get; set; }
+        public string compatibilidade_modelo_tv { get; set; }
+        public decimal preco { get; set; }
+
+        public int estoque { get; set; }
+
+
+        public int id_marca { get; set; }
+        public DateTime datadeutilizacao { get; set; }
+        public DateTime datadocadastro { get; set; }
+
+
     }
 }
