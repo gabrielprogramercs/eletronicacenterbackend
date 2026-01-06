370b7f59329936cccdc32b6f75d89a54044d6471
Gabriel Lopes <gabriel-fernando2008@hotmail.com>
2026-01-06 20:16:11 -0300

    mapeamento do banco de dados em classes no c#

--- eletronicacenterbackend.model/Model/placas_tv.cs
+++ eletronicacenterbackend.model/Model/placas_tv.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using eletronicacenterbackend.model.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,31 +7,17 @@ using System.Threading.Tasks;
 
 namespace eletronicacenterbackend.model.Model
 {
-    public class barras_led
+    public class placas_tv
     {
         public string codigo_fabricante { get; set; }
         public string compatibilidade_modelo_tv { get; set; }
-        public decimal comprimento_cm { get; set; }
         public DateTime datadeutilizacao { get; set; }
         public DateTime datadocadastro { get; set; }
         public int estoque { get; set; }
-        public int id_barra { get; set; }
-        public int id_marca { get; set; }
+        public int id_placa { get; set; }
+        public int idmarca { get; set; }
         public string modelo { get; set; }
         public decimal preco { get; set; }
-        public int quantidade_leds { get; set; }
-
-  
-     
-      
-
-        
-
-
-        
-        
-
-
+        public string tipo { get; set; }
     }
-   
 }
