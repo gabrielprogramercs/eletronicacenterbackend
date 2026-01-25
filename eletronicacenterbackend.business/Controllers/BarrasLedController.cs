ce47eb39cda29fce820d857e21ccf27215756672
Vinícius Luiz Lopes <viniciuslopesti@hotmail.com>
2026-01-25 17:49:24 -0300

    Adjust conflicts and fix entitie barras_led

---
+++ eletronicacenterbackend.business/Controllers/BarrasLedController.cs
@@ -0,0 +1,16 @@
+﻿using eletronicacenterbackend.model.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eletronicacenterbackend.business.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BarrasLedController : ControllerBase
+    {
+        public IActionResult GetBarras()
+        {
+            var barras = new List<barras_led> { };
+            return Ok();
+        }
+    }
+}
--- eletronicacenterbackend.model/Model/barras_led.cs
+++ eletronicacenterbackend.model/Model/barras_led.cs
@@ -1,31 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace eletronicacenterbackend.model.Model
 {
+    [Table("barras_led")]
     public class barras_led
     {
+        [Key]
+        public int id_barra { get; set; }
         public string codigo_fabricante { get; set; }
         public string compatibilidade_modelo_tv { get; set; }
         public decimal comprimento_cm { get; set; }
         public DateTime datadeutilizacao { get; set; }
         public DateTime datadocadastro { get; set; }
         public int estoque { get; set; }
-        public int id_barra { get; set; }
         public int id_marca { get; set; }
         public string modelo { get; set; }
         public decimal preco { get; set; }
         public int quantidade_leds { get; set; }
-        public string codigo_fabricante { get; set; }
-        public string compatibilidade_modelo_tv { get; set; }
-        public decimal preco { get; set; }
-        public int estoque { get; set; }
-        public int id_marca { get; set; }
-        public DateTime datadeutilizacao { get; set; }
-        public DateTime datadocadastro { get; set; }
     }
    
 }
